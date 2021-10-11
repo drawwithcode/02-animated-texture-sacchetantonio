5c41e98b150c820104d297be33a564efb7d9aecf
sacchetantonio <antonio.sacchet@mail.polimi.it>
2021-10-11 22:34:29 +0200

    Prova2

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ function setup() {
 }
 let seed = 0;
 let increment = 0.005;
+let c1 = true;
 
 function draw() {
   seed += increment;
@@ -31,26 +32,37 @@ function draw() {
     noise(seed + 220) * 255
   );
 
-  let mColor = color1;
-
   background(color2);
   for (let x = 50; x < width - 50; x += 20) {
     for (let y = 50; y < height - 50; y += 20) {
-      noStroke();
+      // noStroke();
 
-      fill(mColor);
+      // fill(mColor);
 
-      rect(x, y, 9, 9, 2);
+      // rect(x, y, 9, 9, 2);
       // rect(x, y, 20);
       // rotate(frameCount);
+
+      if (c1 === true) {
+        fill(color1);
+        // background(color2);
+      } else {
+        fill(color3);
+        // background(color4);
+      }
+      push();
+      noStroke();
+
+      rect(x, y, 9, 9, 2);
+      pop();
     }
   }
 }
 function mouseClicked() {
-  if (mColor === color1) {
-    mColor = color3;
+  if (c1 === true) {
+    c1 = false;
   } else {
-    mColor = 255;
+    c1 = true;
   }
 }
 
